b72c2a362d9bc2ab9d07cf37c1b0c58f40302f17
Kristopher Rollert <krollert@ucsc.edu>
2019-11-03 20:23:37 -0800

    finished section 1.4 and 1.5. Completed exercises 5 through 9

---
+++ go/textbook/chp1/excs6/main.go
@@ -0,0 +1,74 @@
+// Exercise 1.6: Modify the Lissajous program to produce images in multiple
+// colors by adding more values to palette and then displaying them by changing
+// the third argument of SetColorIndex in some interesting way.
+//
+// My interesting way was alternating between green, red, and blue lines
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0xFF, 0x00, 0x00, 0xff},
+	color.RGBA{0x00, 0xFF, 0x00, 0xff},
+	color.RGBA{0x00, 0x00, 0xFF, 0xff},
+}
+
+const (
+	blackIndex = 3 // first color in palette
+	redIndex   = 0 // next color in palette
+	blueIndex  = 1
+	greenIndex = 2
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				uint8(math.Mod(float64(i), 3))+1)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
