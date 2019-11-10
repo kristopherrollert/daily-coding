8abb4d4750ebcc613ce0334ff5acc72dea0e28ff
Kristopher Rollert <krollert@ucsc.edu>
2019-11-10 12:24:18 -0800

    actually finished the assingment

--- go/textbook/chp2/excs3/main.go
+++ go/textbook/chp2/excs3/main.go
@@ -1,6 +1,11 @@
 // Exercise 2.3: Rewrite PopCount to use a loop instead of a single expression.
 // Compare the performance of the two versions.
-package popcount
+package main
+
+import (
+	"fmt"
+	"time"
+)
 
 // pc[i] is the population count of i.
 var pc [256]byte
@@ -11,12 +16,44 @@ func init() {
 	}
 }
 
+func main() {
+	var start time.Time
+	var totalLoop time.Duration
+	var totalNormal time.Duration
+	for i := 1; i < 1000000; i++ {
+		start = time.Now()
+		PopCountNormal(100)
+		totalNormal += time.Since(start)
+	}
+
+	for j := 1; j < 1000000; j++ {
+		start = time.Now()
+		PopCountLoop(100)
+		totalLoop += time.Since(start)
+	}
+
+	fmt.Printf("Avergae Normal runtime: %q\n", (totalNormal)/100)
+	fmt.Printf("Avergae Loop runtime: %q\n", (totalLoop)/100)
+
+}
+
 // PopCount returns the population count (number of set bits) of x.
-func PopCount(x uint64) int {
+func PopCountLoop(x uint64) int {
 	var total byte
 	var i uint64
 	for i = 1; i < 8; i++ {
 		total += pc[byte(x>>(i*8))]
 	}
 	return int(total)
+}
+
+func PopCountNormal(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
 }
