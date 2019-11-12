087420be2eeb3cffeb7f6473015a6cd13d46886e
Kristopher Rollert <krollert@ucsc.edu>
2019-11-11 21:54:47 -0800

    fixed exercise description

--- go/textbook/chp2/excs5/main.go
+++ go/textbook/chp2/excs5/main.go
@@ -1,6 +1,6 @@
-// Exercise 2.4: Write a version of PopCount that counts bits by shifting its
-// argument through 64 bit positions, testing the rightmost bit each time.
-// Compare its performance to the table-lookup version.
+// Exercise 2.5: The expression x&(x-1) clears the rightmost non-zero bit of x.
+// Write a version of PopCount that counts bits by using this fact, and assess
+// its performance.
 package main
 
 import (
