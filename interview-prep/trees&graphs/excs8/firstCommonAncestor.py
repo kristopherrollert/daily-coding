f1e9b795dac1a48aa62158c3f53d8a0af9e4f17a
Kristopher Rollert <krollert@ucsc.edu>
2019-12-05 20:32:17 -0800

    once again: this is not finished but I will finish it before I go to bed, I just really want to go climbing right now

---
+++ interview-prep/trees&graphs/excs8/firstCommonAncestor.py
@@ -0,0 +1,7 @@
+# set up linked lists from root to node1 and node2
+# then you can traverse the linked lists and compare elements till they match
+# don't match, then the previous was the most in common.
+
+# how do you make these linked lists.
+# traversal. left then right. If none pop off of linked list.
+# how about binary instead? left is 0 right is 1? 
